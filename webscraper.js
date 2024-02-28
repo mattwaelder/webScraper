f993ed148037041378302a33024dfe487c8a3a0e
mattwaelder <mattwaelder@gmail.com>
2024-02-27 23:10:32 -0800

    initial commit

---
+++ webscraper.js
@@ -0,0 +1,27 @@
+// import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer");
+
+const pageURL = "https://disneyland.disney.go.com/admission/tickets/dates/";
+const selecteDate = "2024-02-28";
+const xpath = '//*[@id="calendar0"]/com-calendar-date';
+
+async function scrapeWeb(url, xpath) {
+  console.log("scraping!");
+
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(url);
+
+  const [el] = await page.$x(xpath);
+  const date = await el.getProperty("date");
+  const dateTxt = await date.jsonValue();
+
+  console.log(dateTxt);
+
+  browser.close();
+}
+
+scrapeWeb(pageURL, xpath);

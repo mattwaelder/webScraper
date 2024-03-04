0272dce1c0a5fc39dc78aab05a60c369be76df4e
Matt Waelder <mattwaelder@gmail.com>
2024-03-03 20:31:17 -0800

    working on how im going to select elements from the page, everything seems to recommend xpath, i need to find out why

--- webscraper.js
+++ webscraper.js
@@ -2,24 +2,28 @@ import puppeteer from "puppeteer";
 // const puppeteer = require("puppeteer");
 // import puppeteer from "puppeteer-core";
 
-const pageURL = "https://disneyland.disney.go.com/admission/tickets/dates/";
+const pageURL = "https://disneyland.disney.go.com/availability-calendar/";
 const selecteDate = "2024-02-28";
-const xpath = '//*[@id="calendar0"]/com-calendar-date';
+// const xpath = '//*[@id="calendar0"]/com-calendar-date[23]';
+// let xpathIcon = "//*[@id="calendar0"]/com-calendar-date[24]/div/div[1]/div/com-icon[2]"
 
-async function scrapeWeb(url, xpath) {
-  console.log("scraping!");
+async function scrapeWeb(url) {
+  console.log("scraping!!!!", url);
 
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const version = await page.browser().version();
-  console.log(version);
+  // console.log(version);
 
   // Navigate the page to a URL
   await page.goto(url);
 
-  const [el] = await page.$x(xpath);
+  const el = await page.waitForSelector("h1 > .header-container");
+  // const [el] = await page.$x(xpath);
+  console.log(el);
   const date = await el.getProperty("date");
+  console.log(date);
   const dateTxt = await date.jsonValue();
 
   console.log(dateTxt);
@@ -27,4 +31,4 @@ async function scrapeWeb(url, xpath) {
   browser.close();
 }
 
-scrapeWeb(pageURL, xpath);
+scrapeWeb(pageURL);
